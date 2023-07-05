6769b119f4ffdffad988e9735d03d4e1aee34948
root <febinraj97@gmail.com>
2023-07-05 10:50:35 +0000

    Initial commit

---
+++ .bash_history
@@ -0,0 +1,25 @@
+ll
+exit
+ssh-keygen -t rsa
+ll
+cd /home/slave/
+ll
+cd ss
+ll
+cd .ssh/
+ll
+cat id_rsa.pub > authorized_keys
+ll
+chmod 700 authorized_keys
+cat authorized_keys 
+ll
+ls
+cd /home/slave/
+ls
+ll
+cd .ssh/
+ll
+cat id_rsa
+ll
+cat id_rsa.pub 
+vi id_rsa.pub 
--- test.js
+++ test.js
@@ -31,8 +31,8 @@ describe("Test2", () => {
 	console.log( "executes before every test" );
 	});
 	
-	it("Is returning 4 when adding 2 + 3", () => {
-	assert.equal(2 + 3, 5);
+	it("Is returning 3 when adding 2 + 2", () => {
+	assert.equal(2 + 2, 4);
 	});
 
 	it("Is returning 8 when multiplying 2 * 4", () => {
